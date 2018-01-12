889e7b81ff45124195b3860bb1eddea982847bea
Magouin <maximegouin2009@hotmail.fr>
2018-01-12 16:52:04 -0500

    Pouet

--- srcs/main.c
+++ srcs/main.c
@@ -22,7 +22,7 @@ void *open_file(char *file, char *argv, size_t *size)
 	else
 	{
 		*size = buf.st_size;
-		if ((ret = mmap(0, buf.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
+		if ((ret = mmap(0, buf.st_size + 1000, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0)) == MAP_FAILED)
 		{
 			close(fd);
 			dprintf(2, "%s: error: Can't load %s in memory.\n", argv, file);
@@ -68,6 +68,14 @@ int main(int ac, char **av)
 	Elf64_Shdr	*s_hdr;
 	Elf64_Phdr	*p_hdr;
 	size_t size;
+	void	*section_text_offset;
+char shellcode[] = "\xeb\x19\x31\xc0\xb0\x04\x31\xdb"
+                   "\xb3\x01\x59\x31\xd2\xb2\x12\xcd"
+                   "\x80\x31\xc0\xb0\x01\x31\xdb\xb3"
+                   "\x01\xcd\x80\xe8\xe2\xff\xff\xff"
+                   "\x20\x79\x30\x75\x20\x73\x70\x33"
+                   "\x34\x6b\x20\x31\x33\x33\x37\x20"
+                   "\x3f\x20";
 
 	if (ac != 2)
 	{
@@ -84,7 +92,7 @@ int main(int ac, char **av)
 		dprintf(2, "%s: error: File architecture for %s not suported. x86_64 only\n", av[0], av[1]);
 		return (3);
 	}
-
+	hdr->e_entry = (size_t)(size);
 	str_tab = get_shstrtab(file, hdr);
 
 	int	nbr = 0;
@@ -93,8 +101,13 @@ int main(int ac, char **av)
 		s_hdr = file + hdr->e_shoff + nbr * hdr->e_shentsize;
 		if (ft_strequ(s_hdr->sh_name + str_tab, ".text"))
 		{
-			printf("section text offset : %p\n", s_hdr->sh_offset);
+			printf("section text offset : %p\n", (void*)s_hdr->sh_offset);
+			section_text_offset = s_hdr->sh_offset;
 		}
+		if (nbr == hdr->e_shnum - 1)
+		{
+		}
+		printf("section : %s - offset : %p - size : %d\n", s_hdr->sh_name + str_tab, (void*)s_hdr->sh_offset, s_hdr->sh_size);
 		nbr++;
 	}
 
@@ -102,12 +115,23 @@ int main(int ac, char **av)
 	while (nbr < hdr->e_phnum)
 	{
 		p_hdr = file + hdr->e_phoff + nbr * hdr->e_phentsize;
-		printf("offset : %p - size : %X\n", p_hdr->p_offset, p_hdr->p_filesz);
+		printf("offset : %p - size : %X\n", (void*)p_hdr->p_offset, p_hdr->p_filesz);
+		printf("vaddr : %p - paddr : %X\n", p_hdr->p_vaddr, p_hdr->p_paddr);
 		// if (nbr == 2)
 		// {
-		p_hdr->p_flags |= PF_W;
+		if (p_hdr->p_offset <= section_text_offset && p_hdr->p_offset + p_hdr->p_filesz >= section_text_offset)
+		{
+			printf("program of .text -\n");
+			p_hdr->p_flags |= PF_W;
+			p_hdr->p_flags |= PF_W;
+		}
+		if (nbr == hdr->e_phnum - 1)
+		{
+		}
 		// }
+		printf("\n");
 		nbr++;
 	}
-	write_woody(file, size);
+	ft_memcpy(file + size, shellcode, sizeof(shellcode));
+	write_woody(file, size + 1000);
 }
