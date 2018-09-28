ec0780d8742bd58d4f21b719cdd47f422021d923
Jeremy CAMHI <jcamhi@student.42.fr>
2018-09-28 21:05:02 +0200

    setup_docker.sh

---
+++ setup_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if !(command -v docker >&-) || !(command -v docker-machine >&-) ; then
+	echo -e "\033[0;31mError: \033[0mPlease install the docker and docker-machine binaries on your system."
+	exit 1
+fi
+
+if ! (docker-machine ls | grep Char >&-) ; then
+	echo "The Char machine has to be created."
+	docker-machine create Char --driver virtualbox || exit 2
+fi
+
+if ! (docker-machine ls | grep Char | grep Running >&- ) ; then
+	echo "The Char machine has to be started"
+	docker-machine start Char || exit 3
+fi
+
+echo -e "\nYou should run the command eval \$(docker-machine env Char) before running ./run.sh\n"
+eval $(docker-machine env Char)
+docker build . -t woody || exit 4
